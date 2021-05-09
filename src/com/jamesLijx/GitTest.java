a5b9f1db57416230fdbae4b791671271e612a923
lijx33137 <lijx33137@hundsun.com>
2021-05-09 17:59:05 +0800

    提交V4版本

---
+++ src/com/jamesLijx/GitTest.java
@@ -0,0 +1,9 @@
+package com.jamesLijx;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+        System.out.println("HelloGitHub");
+        System.out.println("HelloGitee");
+    }
+}
