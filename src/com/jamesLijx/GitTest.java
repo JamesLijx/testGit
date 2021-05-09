4b8130d5e222a8ce4f3a8d6d48f10c754ef40c6a
lijx33137 <lijx33137@hundsun.com>
2021-05-09 17:59:05 +0800

    HotFix提交版本

---
+++ src/com/jamesLijx/GitTest.java
@@ -0,0 +1,10 @@
+package com.jamesLijx;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+        System.out.println("HelloGitHub");
+        System.out.println("HelloGitee");
+        System.out.println("HelloHotFix");
+    }
+}
