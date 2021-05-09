aef202d658e1e3fffeea9ba6f3bdeae7a68a0ace
lijx33137 <lijx33137@hundsun.com>
2021-05-09 19:26:03 +0800

    Master添加了一些判定条件

--- src/com/jamesLijx/GitTest.java
+++ src/com/jamesLijx/GitTest.java
@@ -6,5 +6,10 @@ public class GitTest {
         System.out.println("HelloGitHub");
         System.out.println("HelloGitee");
         System.out.println("HelloHotFix");
+        // 以下是master分支的代码
+        int a = 10;
+        if (a > 10){
+            System.out.println("大于10的整数");
+        }
     }
 }
